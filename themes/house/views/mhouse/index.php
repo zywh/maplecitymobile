ce99d064b9a5102183efc487ace15a8b475eb481
zhengying@yahoo.com <zhengying@yahoo.com>
2016-04-17 21:10:05 +0800

    autobackup 201604172110

--- themes/house/views/mhouse/index.php
+++ themes/house/views/mhouse/index.php
@@ -131,171 +131,23 @@ $(document).on("pageshow","#page_main",function(){
 	$("select").change(function () {
 		getFieldValues(); //Get updated Select
 		//$("#pricetext").text(options["sr"] + ":" +  options['type']); 
-		
+		$('#search_clear').show(); 
 		update_houselist(options);
 		
 		
 	  
 	});
-	 
+	//Search Clear
+	$('#search_clear').click(function()	{ 
+		$("select").val([]);
+		$("select").selectedIndex = -1;
+		$("select").selectmenu('refresh');
+		console.log("Clear Select");
+		$('#search_clear').hide(); 
+		//return false; 
+	});	
 
 	//Detect scroll to bottom
 	$(window).scroll(function(){
 		if($(document).height() > $(window).height()) {
-			if($(window).scrollTop() == $(document).height() - $(window).height()){
-			  //alert("The Bottom");
-			  
-			  if ( loading == false ) {
-			  
-              $('.animation_image').show(); //show loading image
-			  ++page;
-			  console.log("Refresh Page:" + page);
-			  options["pageindex"] = page;
-			  update_houselist(options);
-			  }
-
-		 
-		   }
-		}
-	});
- 
-});
-
-  
-</script>
-
-
-<div data-role="panel" id="panel-city" data-display="overlay" data-position-fixed="true">
-	
-	<ul id="citysearch" class="ui-shadow ui-mini" data-role="listview" data-inset="true" data-filter="true" data-filter-placeholder="输入城市 中/英文" data-filter-theme="a"  ></ul>
-	<select name="province" id="province" data-native-menu="false" data-iconpos="noicon">
-		<option >省份</option>
-		<option value="3">安省</option>
-		<option value="4">BC省</option>
-		<option value="5">阿尔伯塔</option>
-		<option value="6">新不伦瑞克</option>
-		<option value="7">新斯科舍省</option>
-		<option value="8">爱德华王子岛省</option>
-		<option value="9">纽芬兰及拉布拉多</option>
-	</select>	
-
-</div>
-
-<!-- 房源搜索列表开始 -->
-<p></p>
-<div id="house-search"  class="search-area " >
-
-
-<!-- search row1 start -->
-<div class="ui-grid-c" >
-	<div class="ui-block-a">
-		<a href="#panel-city" class="ui-select ui-btn">地区</a>
-	</div>
-    <div class="ui-block-b">
-		<select name="type" id="type" data-corners="false" data-native-menu="false" data-iconpos="noicon" style=>
-			<option >房型</option>
-			<option value="1" >独栋别墅</option>
-			<option value="2">联排别墅</option>
-			<option value="3">豪华公寓</option>
-			<option value="4">双拼别墅</option>
-			<option value="5">度假屋</option>
-			<option value="6">农场</option>
-			<option value="7">空地</option>
-			<option value="8">其他</option>
-		</select>
-	</div>
-	<div class="ui-block-c">	
-		<select name="price" id="price" data-corners="false" data-native-menu="false" data-iconpos="noicon">
-			<option >价格</option>
-			<option value="0-30" >30万以下</option>
-			<option value="30-50" >30-50万</option>
-			<option value="50-100" >50-100万</option>
-			<option value="100-150" >100-150万</option>
-			<option value="150-300" >150-300万</option>
-			<option value="300-400" >300-450万</option>
-			<option value="450-500" >450-600万</option>
-			<option value="600-0" >600以上</option>
-		</select>
-	</div>
-
-	<div class="ui-block-d">		
-		<select name="bedroom" id="bedroom" data-corners="false" data-native-menu="false"  data-iconpos="noicon">
-			<option >卧室</option>
-			<option value="1"> &gt1 </option>
-			<option value="2"> &gt2 </option>
-			<option value="3"> &gt3 </option>
-			<option value="4"> &gt4 </option>
-			<option value="5"> &gt5 </option>
-		</select>
-	</div>
-	<div class="ui-block-a">	
-
-		<select name="washroom" id="washroom"  data-corners="false" data-native-menu="false" data-iconpos="noicon" >
-			<option > 洗手间</option>
-			<option value="1"> &gt1 </option>
-			<option value="2"> &gt2 </option>
-			<option value="3"> &gt3 </option>
-			<option value="4"> &gt4 </option>
-		</select>
-
-	</div>
-
-	<div class="ui-block-b">	
-		<select name="housearea" id="housearea"  data-corners="false"  multiple="multiple" data-native-menu="false"  data-iconpos="noicon">
-			<option >房屋尺寸</option>
-			<option value="0-700" >700平方尺以下</option>
-			<option value="700-1100">700-1100平方尺</option>
-			<option value="1100-1500">1100-1500平方尺</option>
-			<option value="1500-2000">1500-2000平方尺</option>
-			<option value="2000-2500">2000-2500平方尺</option>
-			<option value="2500-3000">2500-3000平方尺</option>
-			<option value="3000-3500">3000-3500平方尺</option>
-			<option value="3500-4000">3500-4000平方尺</option>
-			<option value="4000-0">4000以上</option>
-		</select>
-	</div>
-	<div class="ui-block-c">	
-		<select name="landarea" id="landarea" multiple="multiple"  data-corners="false" data-native-menu="false" data-iconpos="noicon">
-			<option >土地尺寸</option>
-			<option value="0-2000" >2000平方尺以下</option>
-			<option value="2000-4000">2000-4000平方尺</option>
-			<option value="4000-6000">4000-6000平方尺</option>
-			<option value="6000-12000">6000-12000平方尺</option>
-			<option value="12000-435600">12000-1英亩</option>
-			<option value="435600-0">1英亩以上</option>
-		
-		</select>	
-	</div>
-	<div class="ui-block-d">
-		<a href="/" class="ui-select ui-btn">更多</a>
-	</div>	
-</div>		
-<!-- search row2 end -->
-
-
-
-</div>
-<!-- 房源搜索列表结束 -->
-
-
-
-<!-- 地图开始 --> 
-<!-- 地图结束 -->
-
-<!-- 房源列表开始 --> 
-<div data-role="main" class="house_preview ui-content">
-    <span>房源数目:</span><span id="house_count"> </span> 
-    <ul data-role="listview" data-inset="true" data-filter="true" id="house_list" >
-
-    </ul>
-</div>
-<!-- 房源列表结束 -->
-
-<!-- 下一页装载进程图标开始 --> 
-
-<div class="animation_image" style="display:none" align="center">
-	<img src="static/images/ajax-loader2.gif">
-<div>
-
-</div>
-<!-- 下一页装载进程图标结束 --> 
+			if($(window).scrollTop() == $(document).height() - $(window).heigh
