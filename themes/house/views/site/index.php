f9e5c60bbc86df3569d16815c9365469564b9bd9
zhengying@yahoo.com <zhengying@yahoo.com>
2016-04-24 02:10:05 +0800

    autobackup 201604240210

--- themes/house/views/site/index.php
+++ themes/house/views/site/index.php
@@ -13,12 +13,44 @@ $(document).on("pageshow","#page_main",function(){
   
 </script>
 
-<div id="home_page_img" class="home-page-img" >
-<div >
-  <a href="index.php?r=map/index">
-    <img src="/static/images/mapsearch.jpg" >
-  </a>
-  <div class="text">地图搜索</div>
-</div>
 
+<div data-role="main" class="ui-content">
+	<h2>枫之都 - 加拿大房产置业</h2>
+	<ul data-role="listview" data-inset="true">
+	  <li>
+		<a href="index.php?r=mhouse/index&sr=Sale" data-ajax="false" >
+		<img src="chrome.png">
+		<h2>房源搜索</h2>
+		<p>搜索加拿大二手房</p>
+		</a>
+	  </li>
+	  <li>
+		<a href="index.php?r=mhouse/index&sr=Lease" data-ajax="false" >
+		<img src="firefox.png">
+		<h2>租房搜索</h2>
+		<p>搜索</p>
+		</a>
+	  </li>
+	  <li>
+		<a href="index.php?r=map/index" data-ajax="false" >
+		<img src="firefox.png">
+		<h2>地图搜索</h2>
+		<p>搜索地图</p>
+		</a>
+	  </li>
+	  <li>
+		<a href="index.php?r=mhouse/index&sr=Lease" data-ajax="false" >
+		<img src="firefox.png">
+		<h2>学区房</h2>
+		<p>学区房</p>
+		</a>
+	  </li>
+	  <li>
+		<a href="index.php?r=mhouse/index&sr=Lease" data-ajax="false" >
+		<img src="firefox.png">
+		<h2>房源数据统计</h2>
+		<p>房源数据统计</p>
+		</a>
+	  </li>	  
+	</ul>
 </div>
