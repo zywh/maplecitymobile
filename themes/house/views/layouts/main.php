9e558c6877ad4a7077208c3f6b0f58f929d3ed60
zhengying@yahoo.com <zhengying@yahoo.com>
2016-05-19 10:10:08 +0800

    autobackup 201605191010

--- themes/house/views/layouts/main.php
+++ themes/house/views/layouts/main.php
@@ -6,7 +6,7 @@
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
   // return only the headers and not the content
   // only allow CORS if we're doing a GET - i.e. no saving for now.
-  if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']) {
+  if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'])) {
     header('Access-Control-Allow-Origin: *');
     header('Access-Control-Allow-Headers: X-Requested-With');
   }
