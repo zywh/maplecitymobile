7437dc4048e3000af4adde41ef8c347c24a02dfd
zhengying@yahoo.com <zhengying@yahoo.com>
2016-05-21 11:10:10 +0800

    autobackup 201605211110

--- themes/house/views/layouts/main.php
+++ themes/house/views/layouts/main.php
@@ -6,14 +6,14 @@
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
   // return only the headers and not the content
   // only allow CORS if we're doing a GET - i.e. no saving for now.
-  if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'])) {
+  //if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'])) {
     header('Access-Control-Allow-Origin: *');
     header('Access-Control-Allow-Headers: X-Requested-With');
-  }
+  //}
   exit;
 }
 
-//header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: *");
 //header("Access-Control-Allow-Methods: GET, POST, OPTIONS");  
 //header("Access-Control-Allow-Headers: content-type");
 //header('Access-Control-Allow-Headers: Authorization, Content-Type' );    
