9d7c7c979c94263986593c08119595ca6732a78e
zhengying@yahoo.com <zhengying@yahoo.com>
2016-05-22 08:10:07 +0800

    autobackup 201605220810

--- protected/controllers/NggetController.php
+++ protected/controllers/NggetController.php
@@ -2,9 +2,10 @@
 
 class NgGetController extends XFrontBase
 {
-
-   public function actionGetProjects(){
 	
+	public $imghost = "http://m.maplecity.com.cn/";
+   public function actionGetProjects(){
+		//$imghost = "http://m.maplecity.com.cn/";
 		$results = array();
 		$postParms = array();
 		ini_set("log_errors", 1);
@@ -15,18 +16,42 @@ class NgGetController extends XFrontBase
 		$postParms = (!empty($_POST['parms']))?  $_POST['parms'] : array();
 		
 		if (!empty($postParms['id'])){
+			//return single record for detail page
 			$criteria->addCondition('id="'.$_POST['parms']['id'].'"');
-		}
-		//$criteria->addCondition('recommend=1');
-		$subject = Subject::model()->findAll($criteria);
-		foreach($subject as $row){
+			$subject = Subject::model()->findAll($criteria);
+			
+			foreach($subject as $row){
 
-			//$result['id'] = $row["id"]; 
+			$result['id'] = $row["id"]; 
 			$result['name'] = $row["name"]; 
 			$result['summary'] = $row["summary"]; 
+			$result['city_id'] = $row["city_id"]; 
+			$result['room_type_image'] = $imghost.$row["room_type_image"]; 
+			
+			
 			$results[] = $result;
+			}
+		} else {
+			//Return all recommended project
+			
+			$criteria->addCondition('recommend=1');
+			$subject = Subject::model()->findAll($criteria);
+			foreach($subject as $row){
+
+				$result['id'] = $row["id"]; 
+				$result['name'] = $row["name"]; 
+				$result['city_id'] = $row["city_id"]; 
+				$result['room_type_image'] = $imghost.$row["room_type_image"]; 
+				$results[] = $result;
+			}
+			
 		}
-		header('Content-Type: application/json');
+		
+			
+		
+		
+		
+
 		echo json_encode($results);
     }	
 
