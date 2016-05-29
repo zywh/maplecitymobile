4349fb7853ff9ea11e6c67dab4157a7e4df45ef0
zhengying@yahoo.com <zhengying@yahoo.com>
2016-05-29 13:10:07 +0800

    autobackup 201605291310

--- protected/controllers/NggetController.php
+++ protected/controllers/NggetController.php
@@ -484,5 +484,42 @@ class NgGetController extends XFrontBase
 	//Function END  
     }
 	
-	
+	/*
+	REST for About Page content
+	*/
+	public function actionGetAbout(){
+		$_POST = (array) json_decode(file_get_contents('php://input'), true);
+		$postParms = (!empty($_POST['parms']))?  $_POST['parms'] : array();
+		$catalog_id = $postParms('id');
+		if ($catalog_id == '') $catalog_id = 27;
+		if ($catalog_id == 27) $cat_name_en="MAPLECITY PFOFILE";
+		if ($catalog_id == 28) $cat_name_en="SUPERIORITY";
+		if ($catalog_id == 30) $cat_name_en="CONTACT US";
+		if ($catalog_id == 31) $cat_name_en="JOIN US";
+				
+		$prev_post = Post::model()->findAll(array(
+			'select'    => 'id, title, content, view_count',
+			'condition' => ' catalog_id = :catalog_id',
+			'params'    => array(':catalog_id' => $catalog_id),
+			'order'     => 'id ASC',
+			'limit'     => 1
+		));
+				//$prev_post->view_count += 1;
+		//$prev_post->save();
+				
+		$next_post = Post::model()->findAll(array(
+			'select'    => 'id, title',
+			'condition' => 'id < :id AND catalog_id = :catalog_id',
+			'params'    => array(':id' => $id, ':catalog_id' => $catalog_id),
+			'order'     => 'id DESC',
+			'limit'     => 1
+		));
+		$this->render('about', array(
+						'cat_name_en' => $cat_name_en,
+			'post'      => $post,
+			'prev_post' => $prev_post[0],
+			'next_post' => $next_post[0]
+		));
+	}
+		
 }
